0d5f69a1f8175fc4b7c734604e4a471e1d4b09dc
Richard Foster <richard@rpf.me>
2021-02-20 23:37:22 +0000

    Add logging to build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,6 @@
 rm public/assets/dist/bundle.js
+echo 'Cleaned...'
 protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto --ts_proto_out=. ./src/protos/*.proto
+echo 'Built proto typescript files...'
 browserify src/index.ts -p [ tsify ] -o public/assets/dist/bundle.js
+echo 'Built client...'
