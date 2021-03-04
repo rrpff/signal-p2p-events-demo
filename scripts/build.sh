f5051f9b04947ae360da55c0b21361bf81aded9f
Richard Foster <richard@rpf.me>
2021-03-04 02:50:41 +0000

    Migrate to create-react-app

--- scripts/build.sh
+++
@@ -1,6 +0,0 @@
-rm public/assets/dist/bundle.js
-echo 'Cleaned...'
-protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto --ts_proto_out=. ./src/protos/*.proto
-echo 'Built proto typescript files...'
-browserify src/index.ts -p [ tsify ] -o public/assets/dist/bundle.js
-echo 'Built client...'
---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import reportWebVitals from './reportWebVitals'
+
+const App = () => {
+  return (
+    <span>testooooo</span>
+  )
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
+
+if (process.env.NODE_ENV === 'development') {
+  reportWebVitals(console.log)
+}
