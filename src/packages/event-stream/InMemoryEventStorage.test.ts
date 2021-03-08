c138f7812b1751df6a9968fd4984208868f69e5b
Richard Foster <richard@rpf.me>
2021-03-08 21:58:08 +0000

    Change InMemoryEventStorage#connect to #tap
    The 'connect' terminology is slightly confusing. Think this
    makes it clearer that this function hooks into the stream without
    being a stream itself.

--- src/packages/event-stream/InMemoryEventStorage.test.ts
+++ src/packages/event-stream/InMemoryEventStorage.test.ts
@@ -1,38 +1,38 @@
 import EventStream from './EventStream'
 import InMemoryEventStorage from './InMemoryEventStorage'
 
-test('loads stored events into streams when connected', async () => {
+test('loads stored events into streams when tapped', async () => {
   const event = Math.random()
   const storage = new InMemoryEventStorage<number>([event])
   const stream = new EventStream<number>()
   const subscriber = jest.fn()
 
   stream.subscribe(subscriber)
-  await storage.connect(stream)
+  await storage.tap(stream)
 
   expect(subscriber).toHaveBeenCalledWith(event)
 })
 
-test('stores new events from a connected stream in memory', async () => {
+test('stores new events from a tapped stream in memory', async () => {
   const event = Math.random()
   const storage = new InMemoryEventStorage<number>()
   const stream = new EventStream<number>()
 
-  await storage.connect(stream)
+  await storage.tap(stream)
   stream.push(event)
 
   expect(storage.events).toContain(event)
 })
 
-test('stores new events from all connected stream in memory', async () => {
+test('stores new events from all tapped streams in memory', async () => {
   const event1 = Math.random()
   const event2 = Math.random()
   const storage = new InMemoryEventStorage<number>()
   const stream1 = new EventStream<number>()
   const stream2 = new EventStream<number>()
 
-  await storage.connect(stream1)
-  await storage.connect(stream2)
+  await storage.tap(stream1)
+  await storage.tap(stream2)
   stream1.push(event1)
   stream2.push(event2)
 
--- src/packages/event-stream/InMemoryEventStorage.ts
+++ src/packages/event-stream/InMemoryEventStorage.ts
@@ -3,7 +3,7 @@ import { IEventStream } from './interfaces'
 export default class InMemoryEventStorage<TEvent> {
   constructor(public events: TEvent[] = []) {}
 
-  async connect(stream: IEventStream<TEvent>) {
+  async tap(stream: IEventStream<TEvent>) {
     this.events.forEach(event => stream.push(event))
     stream.subscribe(event => this.events.push(event))
   }
